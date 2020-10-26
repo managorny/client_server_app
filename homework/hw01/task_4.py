3808a3be62a12f4643c5208ed60de931cddf5936
solunasalles <m.a.nagorny@gmail.com>
2020-10-26 11:00:26 +0500

    homework01_new_start

--- homework/hw01/task_4.py
+++ homework/hw01/task_4.py
@@ -1,15 +1,15 @@
 """
-4. Преобразовать слова
- «разработка», «администрирование», «protocol», «standard» из строкового представления в байтовое
- и выполнить обратное преобразование (используя методы encode и decode).
+4. Преобразовать слова «разработка», «администрирование», «protocol»,
+«standard» из строкового представления в байтовое и выполнить
+обратное преобразование (используя методы encode и decode).
 """
 
 my_list = ['разработка', 'администрирование', 'protocol', 'standard']
 
 encode_el = []
 decode_el = []
-for i in my_list:
-    encode_el.append(i.encode('utf-8'))
+for itm in my_list:
+    encode_el.append(itm.encode('utf-8'))
 
 for el in encode_el:
     decode_el.append(el.decode('utf-8'))
